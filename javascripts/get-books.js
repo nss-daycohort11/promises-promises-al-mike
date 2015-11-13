1257741e3339c3ad318d318fbfb3add9c2132c83
albennett <amyleeannebennett@gmail.com>
2015-11-13 11:35:26 -0600

    changes with emma

--- javascripts/get-books.js
+++ javascripts/get-books.js
@@ -3,25 +3,6 @@ define(function(require) {
   var $ = require("jquery");
   var Q = require("q");
 
-  
-  return{
-    getBooks: function() {  
-      var deferred = Q.defer();
-      $.ajax("https://nss-book-store.firebaseio.com/books.json")
-        .done(function(booksjson) {
-          console.log("books json", booksjson);
-          deferred.resolve(booksjson);
-        })
-        .fail(function(xhr, status, error) {
-      // Since the call failed, we have to reject the promise
-          deferred.reject(error);
-          console.log("error", error);
-        });
-        return deferred.promise;
-
-  var Q = require("q");
-  var booktypes = require("get-book-types");
-
   return {
     loadBooks: function() {
       // console.log("loading books");
